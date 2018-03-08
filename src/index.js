7a232a0ec9f472703474c5b97ccb082f705562b4
NataliaKirishun <31582169+NataliaKirishun@users.noreply.github.com>
2018-03-08 23:38:45 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,73 @@
 class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
-  }
+    constructor(initialValue) {
+        this.operandsAndOperators = [initialValue]
+    }
 
-  add(number) {
-    // your implementation
-  }
-  
-  subtract(number) {
-    // your implementation
-  }
+    add(number) {
+        this.operandsAndOperators.push('+');
+        this.operandsAndOperators.push(number);
+        return this;
+    }
 
-  multiply(number) {
-    // your implementation
-  }
+    subtract(number) {
+        this.operandsAndOperators.push('-');
+        this.operandsAndOperators.push(number);
+        return this;
+    }
 
-  devide(number) {
-    // your implementation
-  }
+    multiply(number) {
+        this.operandsAndOperators.push('*');
+        this.operandsAndOperators.push(number);
+        ;
+        return this;
+    }
 
-  pow(number) {
-    // your implementation
-  }
-}
+    devide(number) {
+        this.operandsAndOperators.push('/');
+        this.operandsAndOperators.push(number);
+        return this;
+    }
 
+    pow(number) {
+        this.operandsAndOperators.push('^');
+        this.operandsAndOperators.push(number);
+        return this;
+    }
+
+    valueOf() {
+        let result;
+        for (let j = this.operandsAndOperators.length - 2; j > 0; j--) {
+            if (this.operandsAndOperators[j] === '^') {
+                result = Math.pow(this.operandsAndOperators[j - 1], this.operandsAndOperators[j + 1]);
+                this.operandsAndOperators.splice(j - 1, 3, result);
+                j += 2;
+            }
+        }
+        let operators = ['/', '*', '-', '+'];
+        for (let i = 0; i < operators.length; i++) {
+            for (let j = 0; j < this.operandsAndOperators.length - 1; j++) {
+                if (operators[i] === this.operandsAndOperators[j]) {
+                    switch (operators[i]) {
+                        case'/':
+                            result = this.operandsAndOperators[j - 1] / this.operandsAndOperators[j + 1];
+                            break;
+                        case'*':
+                            result = this.operandsAndOperators[j - 1] * this.operandsAndOperators[j + 1];
+                            break;
+                        case'-':
+                            result = this.operandsAndOperators[j - 1] - this.operandsAndOperators[j + 1];
+                            break;
+                        case'+':
+                            result = this.operandsAndOperators[j - 1] + this.operandsAndOperators[j + 1];
+                            console.log(result);
+                            break;
+                    }
+                    this.operandsAndOperators.splice(j - 1, 3, result);
+                    j = 0;
+                }
+            }
+        }
+        return this.operandsAndOperators[0];
+    }
+};
 module.exports = SmartCalculator;
